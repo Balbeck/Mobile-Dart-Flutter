3e451049ed7f1095294f9a7486a8cd3c0415cb29
HectorAlbecker <hector.albecker@sanofi.com>
2025-08-15 04:20:27 +0200

    feat: Module01

---
+++ mobileModule01/weather_app/lib/main.dart
@@ -0,0 +1,56 @@
+// ... existing code ...
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/currently_screen.dart'; // Ajoute ces imports
+import 'package:weather_app/screens/today_screen.dart';
+import 'package:weather_app/screens/week_screen.dart';
+import 'widgets/top_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String city = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3, // 3 tabs
+      child: Scaffold(
+        appBar: TopBar(
+          onSearch: (value) => setState(() => city = value.trim()),
+          onGeo: () => setState(() => city = '42_Paris'),
+        ),
+
+        body: TabBarView(
+          children: [
+            CurrentlyScreen(city: city),
+            TodayScreen(city: city),
+            WeekScreen(city: city),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.access_time), text: 'Currently'),
+              Tab(icon: Icon(Icons.today), text: 'Today'),
+              Tab(icon: Icon(Icons.calendar_today), text: 'Weekly'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
